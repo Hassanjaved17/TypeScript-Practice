723a9ce7fe338c13664cea290a445849324c2cc5
Hassanjaved17 <hassanaptech9@gmail.com>
2026-04-07 00:09:06 +0500

    "practice ts"

--- typescript.ts
+++ typescript.ts
@@ -1,13 +1,15 @@
 // Primitive Types
 // string, nunber, boolean
 
+// Explicit Type
 // var userName: string = "hassan";
 // var age: number = 30;
 // var isMarried: boolean = false;
-
 // console.log("User Name: " + userName + ", Age: " + age + ", Is Married: " + isMarried);
 
 function greet(name: string): string { // Function with type annotations
     return "Hello, " + name + "!" // String interpolation 
 }   
 console.log(greet("John"));// Calling the function
+
+

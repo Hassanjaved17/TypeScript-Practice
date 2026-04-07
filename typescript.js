39cb3b97230f6524ca43da5044d1bb3d60c45e52
Hassanjaved17 <hassanaptech9@gmail.com>
2026-04-07 21:54:31 +0500

    practice ts

--- typescript.js
+++ typescript.js
@@ -1,11 +1,17 @@
 "use strict";
 // Primitive Types
 // string, nunber, boolean
+// Explicit Type
 // var userName: string = "hassan";
 // var age: number = 30;
 // var isMarried: boolean = false;
 // console.log("User Name: " + userName + ", Age: " + age + ", Is Married: " + isMarried);
-function greet(name) {
-    return "Hello, " + name + "!"; // String interpolation 
-}
-console.log(greet("John")); // Calling the function
+// function greet(name: string): string { // Function with type annotations
+//     return "Hello, " + name + "!" // String interpolation 
+// }   
+// console.log(greet("John"));// Calling the function
+// implicit Type or inference
+var userName = "hassan"; // TypeScript infers the type as string
+var age = 20; // TypeScript infers the type as number
+var isMarried = false; // TypeScript infers the type as boolean
+console.log("User Name: " + userName + ", Age: " + age + ", Is Married: " + isMarried);
--- typescript.ts
+++ typescript.ts
@@ -7,10 +7,14 @@
 // var isMarried: boolean = false;
 // console.log("User Name: " + userName + ", Age: " + age + ", Is Married: " + isMarried);
 
+// function greet(name: string): string { // Function with type annotations
+//     return "Hello, " + name + "!" // String interpolation 
+// }   
+// console.log(greet("John"));// Calling the function
 
-function greet(name: string): string { // Function with type annotations
-    return "Hello, " + name + "!" // String interpolation 
-}   
-console.log(greet("John"));// Calling the function
-
+// implicit Type or inference
+var userName = "hassan"; // TypeScript infers the type as string
+var age = 20; // TypeScript infers the type as number
+var isMarried = false; // TypeScript infers the type as boolean
+console.log("User Name: " + userName + ", Age: " + age + ", Is Married: " + isMarried);
 

13423adc3fbff9d6f7e6c3d5b8dad097ec444160
Hassanjaved17 <hassanaptech9@gmail.com>
2026-04-09 21:36:39 +0500

    practice typescript

--- typescript.ts
+++ typescript.ts
@@ -15,20 +15,20 @@
 // console.log(greet("John"));// Calling the function
 
 // implicit Type or inference
-// implicit type is when TypeScript infers the type of a variable based on the assigned value
-var userName = "hassan"; // TypeScript infers the type as string
-var age = 20; // TypeScript infers the type as number
-var isMarried = false; // TypeScript infers the type as boolean
-console.log("User Name: " + userName + ", Age: " + age + ", Is Married: " + isMarried);
+// // implicit type is when TypeScript infers the type of a variable based on the assigned value
+// var userName = "hassan"; // TypeScript infers the type as string
+// var age = 20; // TypeScript infers the type as number
+// var isMarried = false; // TypeScript infers the type as boolean
+// console.log("User Name: " + userName + ", Age: " + age + ", Is Married: " + isMarried);
 
 // special types (any)
 // any type can hold any value and bypasses type checking
- var username: any = "hassan"; // Initially a string
- console.log("Username (string): " + username);
- username = 10;
- console.log("Username (number): " + username);
- username = true;
- console.log("Username (boolean): " + username);
+//  var username: any = "hassan"; // Initially a string
+//  console.log("Username (string): " + username);
+//  username = 10;
+//  console.log("Username (number): " + username);
+//  username = true;
+//  console.log("Username (boolean): " + username);
 
 //  TypeScript Types (for objects)
 // tuple type (fixed-length array with different types, order is important and length is fixed)
@@ -37,14 +37,18 @@ console.log("User Name: " + userName + ", Age: " + age + ", Is Married: " + isMa
 var person: [string, number] = ["hassan", 20]; // Tuple with a string and a number
 console.log("Name: " + person[0] + ", Age: " + person[1]);
 
+var uname: [string, number] = ["Albert", 30];
+uname.push("Einstein"); // This will cause an error because the tuple is defined to have only two elements
+console.log(uname); // This will cause an error because the tuple is defined to have only two elements
+
 
 // enum type
 // An enum (short for "enumeration") is a way to define a set of named constants. It allows you to create a collection of related values that can be used in a more readable and maintainable way.
-enum Color {
-    Red, // 0
-    Green, // 1 
-    Blue // 2
-}
-var favoriteColor: Color = Color.Green;
-console.log("Favorite Color: " + favoriteColor);
+// enum Color {
+//     Red, // 0
+//     Green, // 1 
+//     Blue // 2
+// }
+// var favoriteColor: Color = Color.Green;
+// console.log("Favorite Color: " + favoriteColor);
 
